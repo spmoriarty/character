532683aaa995a50bbe5ca4cbef6eb44cbe493c52
Sean Moriarty <spmoriarty1@gmail.com>
2022-04-27 15:50:19 -0700

    Added styles for dropdowns

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 // import functions and grab DOM elements
 const headDropdown = document.getElementById('head-dropdown');
+
+
 const middleDropdown = document.getElementById('middle-dropdown');
+
 const bottomDropdown = document.getElementById('bottom-dropdown');
+
+
 const headEl = document.getElementById('head');
 const middleEl = document.getElementById('middle');
 const bottomEl = document.getElementById('bottom');
@@ -9,6 +14,8 @@ const reportEl = document.getElementById('report');
 const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
+
+let reportEL= 0;
 
 // set state for how many times the user changes the head, middle, and bottom
 // set state for all of the character's catchphrases
